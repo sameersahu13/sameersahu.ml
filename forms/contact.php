fa3a7e24749401b1f10bc1fc9dbbbc328d75821c
Sameer Sahu <69348276+sameersahu13@users.noreply.github.com>
2021-02-02 19:22:56 +0530

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,34 @@
+<?php
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'sameersahu.trv@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Your message has been sent. Thank you!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'sameersahu.techz@gmail.com',
+    'password' => 'Sameersahu@786',
+    'port' => '587'
+  );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
